9b14556de1de7592aad309426b29b0d53f5599df
jin'ming'chao <441821724@qq.com>
2023-12-15 16:25:31 +0800

    20231215

---
+++ spring-boot-starter/src/main/java/explore/springboot/starter/secure/MvcConfig.java
@@ -0,0 +1,11 @@
+package explore.springboot.starter.secure;
+
+public class MvcConfig implements WebMvcConfigurer{
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+        https://spring.io/guides/gs/securing-web/
+    }
+}
